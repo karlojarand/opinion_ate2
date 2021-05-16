7156ef1b68960cbf2f8f2fbe2fedf303b90dabaf
Karl Ojarand <karl@play.ee>
2021-05-16 23:05:29 +0300

    ´list-restaurants

--- src/components/__tests__/RestaurantList.spec.js
+++ src/components/__tests__/RestaurantList.spec.js
@@ -2,10 +2,31 @@ import {render} from '@testing-library/react';
 import {RestaurantList} from '../RestaurantList';
 
 describe('RestaurantList', () => {
-    it('loads restaurants on first render', () => {
-        const loadRestaurants = jest.fn().mockName('loadRestaurants');
+    const restaurants = [
+            {id: 1, name: 'Sushi Place'},
+            {id: 2, name: 'Pizza Place'},
+          ];
+          let loadRestaurants;
+          let context;
+        
+          beforeEach(() => {
+            loadRestaurants = jest.fn().mockName('loadRestaurants');
+        
+            context = render(
+              <RestaurantList
+                loadRestaurants={loadRestaurants}
+                restaurants={restaurants}
+              />,
+            );
+          });
+        
+    it('displays the restaurants', () => {
 
-        render(<RestaurantList loadRestaurants={loadRestaurants} />);
+        const {queryByText} = context;
+        expect(queryByText('Sushi Place')).not.toBeNull();
+      expect(queryByText('Pizza Place')).not.toBeNull();
+      });
+    it('loads restaurants on first render', () => {
 
         expect(loadRestaurants).toHaveBeenCalled();
     });
